62e672624657989f28ad053c97e2b874b89faa57
Thanos Ploumis <aploumis@redhat.com>
2021-02-09 14:52:21 +0200

    Unify certificates

--- core/src/main/groovy/noe/workspace/WorkspaceAbstract.groovy
+++ core/src/main/groovy/noe/workspace/WorkspaceAbstract.groovy
@@ -124,28 +124,16 @@ abstract class WorkspaceAbstract implements IWorkspace {
    *
    */
   void copyCertificates() {
-//    List<String> certificates = ["server.crt", "server.jks", "server.key", "server.p12"]
-//    String sslStringDir = PathHelper.join(platform.tmpDir, "ssl", "self_signed")
     String sslIntermediateDir = PathHelper.join(platform.tmpDir, "ssl", "proper", "generated", "ca", "intermediate")
     File intermediateTmpDir = new File(sslIntermediateDir)
-//    File sslDir = new File(sslStringDir)
-    String resourcesPath = "ssl/self_signed/"  //resources jar path is always separated by /
     String intermediatePath = "ssl/proper/generated/ca/intermediate/"
-//    if (!sslDir.exists()) {
-//      JBFile.mkdir(sslDir)
-//    }
 
     if (!intermediateTmpDir.exists()) {
       JBFile.mkdir(intermediateTmpDir)
     }
 
-//    JBFile.makeAccessible(sslDir)
     JBFile.makeAccessible(intermediateTmpDir)
 
-//    for (String certName : certificates) {
-//      File certFile = Library.retrieveResourceAsFile("${resourcesPath}${certName}")
-//      JBFile.move(certFile, sslDir)
-//    }
     File sslIntermediateFile = Library.retrieveResourceAsFile("${intermediatePath}")
     JBFile.move(sslIntermediateFile, intermediateTmpDir)
   }
